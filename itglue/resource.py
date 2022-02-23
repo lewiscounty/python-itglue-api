07648cb25d32feeb6c42a3ab57a3f974aa7aa2fe
David Haymond <david.haymond@lewiscountywa.gov>
2022-02-22 16:54:52 -0800

    Refactor Resource and Attribute classes

--- itglue/resource.py
+++ itglue/resource.py
@@ -1,66 +1,204 @@
-class Resource:
+from collections.abc import Collection
+from types import SimpleNamespace
+
+
+class Attribute:
+    """Represents a single IT Glue resource attribute."""
+
+    def __init__(self, network_key=None):
+        """
+        Initialize an Attribute instance with optional custom network_key.
+
+        When Attribute instances are assigned to attributes of Resource
+        subclasses, Attribute.set_name() will be called automatically with
+        the name of the attribute that the instance was assigned to.
+
+        If network_key is omitted, set_name() will also initialize network_key
+        to be the same as the name except with underscores replaced with hyphens.
+        """
+
+        self.name = None
+        self.network_key = network_key
+
+    def set_name(self, name):
+        """
+        Set the Attribute instance name.
+
+        If self.network_key is None, set_name() also sets a default network_key
+        by replacing underscores in the new name with hyphens and assigning
+        the result to self.network_key.
+        """
+
+        self.name = name
+        if self.network_key is None:
+            self.network_key = name.replace('_', '-')
+
+
+class AttributeCollection(Collection):
+    """Store a collection of Attribute instances."""
+
+    def __init__(self):
+        self.__attributes = {}
+
+    def add(self, attr):
+        if attr.name is None:
+            raise ValueError("Cannot add attribute with a name of None: call Attribute.set_name() first")
+        if self.get_by_name(attr.name):
+            raise ValueError(f"Another attribute with the name \"{attr.name}\" already exists")
+        if self.get_by_network_key(attr.network_key):
+            raise ValueError(f"Another attribute with the network key \"{attr.network_key}\" already exists")
+
+        self.__attributes[attr.name] = attr
+
+    def get_by_name(self, name):
+        if name in self.__attributes:
+            return self.__attributes[name]
+        return None
+
+    def get_by_network_key(self, network_key):
+        for val in self.__attributes.values():
+            if val.network_key == network_key:
+                return val
+
+        return None
+
+    def __contains__(self, item):
+        if isinstance(item, Attribute):
+            return item in self.__attributes.values()
+        return item in self.__attributes
+
+    def __iter__(self):
+        return iter(self.__attributes.values())
+
+    def __len__(self):
+        return len(self.__attributes)
+
+
+class ResourceMeta(type):
+    """Metaclass for all Resource subclasses."""
+
+    resource_classes = {}
+
+    def __new__(metaclass, classname, bases, namespace_dict, **kwargs):
+        # Only use custom initialization for subclasses of Resource
+        # (exclude Resource itself)
+        parents = [b for b in bases if isinstance(b, ResourceMeta)]
+        if not parents:
+            # Run default type() constructor
+            return super().__new__(metaclass, classname, bases, namespace_dict, **kwargs)
+
+        attrs = {'Meta': namespace_dict.pop('Meta', SimpleNamespace())}
+        attrs['Meta'].attributes = AttributeCollection()
+
+        for (key, val) in namespace_dict.items():
+            if isinstance(val, Attribute):
+                val.set_name(key)
+                attrs['Meta'].attributes.add(val)
+            else:
+                attrs[key] = val
+
+        # Call type() to create the class and then register it
+        # in the resource_classes list
+        cls = super().__new__(metaclass, classname, bases, attrs, **kwargs)
+        metaclass.resource_classes[attrs['Meta'].resource_type] = cls
+        return cls
+
+    def lookup_subclass(cls, resource_type):
+        """Get the Resource subclass associated with the specified resource_type."""
+
+        return cls.resource_classes[resource_type]
+
+
+class Resource(metaclass=ResourceMeta):
+    """Base class representing an IT Glue resource."""
+
     def __init__(self, id_=None, **kwargs):
+        """
+        Initialize an instance representing an IT Glue resource.
+
+        The id_ positional arg is optional and defaults to None
+        (useful for resources that don't yet have an ID). All other
+        args are keyword-only and specify resource attributes to set.
+        Each Resource subclass defines its own list of valid attributes.
+        """
+
         self.id = id_
 
         for (key, val) in kwargs.items():
-            if key in self.attribute_map:
+            if key in self.Meta.attributes:
                 setattr(self, key, val)
             else:
-                raise TypeError("{} is not a valid keyword argument")
+                raise TypeError(f"\"{key}\" is not a valid keyword argument")
 
-    @classmethod
-    def from_network_dict(cls, network_dict):
-        inverted_map = {v: k for (k, v) in cls.attribute_map.items()}
+    @staticmethod
+    def from_network_dict(network_dict):
+        """
+        Create a Resource instance from a network dict parsed from a raw
+        IT Glue API JSON response.
+
+        The network_dict must have a 'type' key that matches a Resource subclass's
+        declared resource_type. This value is used to ensure that the resulting Resource
+        instance is an instance of the correct Resource subclass.
+        """
+
+        cls = Resource.resource_classes[network_dict['type']]
         kwargs = {'id_': network_dict['id']}
 
         for (key, val) in network_dict['attributes'].items():
-            if key in inverted_map:
-                kwargs[inverted_map[key]] = val
+            attr = cls.Meta.attributes.get_by_network_key(key)
+            if attr:
+                kwargs[attr.name] = val
 
         resource = cls(**kwargs)
-        resource._network_dict = network_dict
+        resource.Meta.network_dict = network_dict
         return resource
 
+
 class Configuration(Resource):
-    attribute_map = {
-        'archived': 'archived',
-        'asset_tag': 'asset-tag',
-        'configuration_status_id': 'configuration-status-id',
-        'configuration_status_name': 'configuration-status-name',
-        'configuration_type_id': 'configuration-type-id',
-        'configuration_type_kind': 'configuration-type-kind',
-        'configuration_type_name': 'configuration-type-name',
-        'contact_id': 'contact-id',
-        'contact_name': 'contact-name',
-        'created_at': 'created-at',
-        'default_gateway': 'default-gateway',
-        'hostname': 'hostname',
-        'installed_at': 'installed-at',
-        'installed_by': 'installed-by',
-        'location_id': 'location-id',
-        'location_name': 'location-name',
-        'mac_address': 'mac-address',
-        'manufacturer_id': 'manufacturer-id',
-        'manufacturer_name': 'manufacturer-name',
-        'model_id': 'model-id',
-        'model_name': 'model-name',
-        'my_glue': 'my-glue',
-        'name': 'name',
-        'notes': 'notes',
-        'operating_system_id': 'operating-system-id',
-        'operating_system_name': 'operating-system-name',
-        'operating_system_notes': 'operating-system-notes',
-        'organization_id': 'organization-id',
-        'organization_name': 'organization-name',
-        'organization_short_name': 'organization-short-name',
-        'position': 'position',
-        'primary_ip': 'primary-ip',
-        'purchased_at': 'purchased-at',
-        'purchased_by': 'purchased-by',
-        'psa_integration': 'psa-integration',
-        'resource_url': 'resource-url',
-        'restricted': 'restricted',
-        'serial_number': 'serial-number',
-        'updated_at': 'updated-at',
-        'warranty_expires_at': 'warranty-expires-at',
-    }
+    """
+    Represents an IT Glue Configuration.
+    """
+
+    class Meta:
+        resource_type = 'configurations'
+
+    archived = Attribute()
+    asset_tag = Attribute()
+    configuration_status_id = Attribute()
+    configuration_status_name = Attribute()
+    configuration_type_id = Attribute()
+    configuration_type_kind = Attribute()
+    configuration_type_name = Attribute()
+    contact_id = Attribute()
+    contact_name = Attribute()
+    created_at = Attribute()
+    default_gateway = Attribute()
+    hostname = Attribute()
+    installed_at = Attribute()
+    installed_by = Attribute()
+    location_id = Attribute()
+    location_name = Attribute()
+    mac_address = Attribute()
+    manufacturer_id = Attribute()
+    manufacturer_name = Attribute()
+    model_id = Attribute()
+    model_name = Attribute()
+    my_glue = Attribute()
+    name = Attribute()
+    notes = Attribute()
+    operating_system_id = Attribute()
+    operating_system_name = Attribute()
+    operating_system_notes = Attribute()
+    organization_id = Attribute()
+    organization_name = Attribute()
+    organization_short_name = Attribute()
+    position = Attribute()
+    primary_ip = Attribute()
+    purchased_at = Attribute()
+    purchased_by = Attribute()
+    psa_integration = Attribute()
+    resource_url = Attribute()
+    restricted = Attribute()
+    serial_number = Attribute()
+    updated_at = Attribute()
+    warranty_expires_at = Attribute()
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -2,8 +2,141 @@ import unittest
 
 from itglue.resource import *
 
+class TestAttribute(unittest.TestCase):
+    """Test the Attribute class."""
+
+    def test_default_constructor(self):
+        """Test constructor with no args."""
+
+        a = Attribute()
+
+        self.assertIsNone(a.name, "name attr not initialized to None")
+        self.assertIsNone(a.network_key, "network_key attr not initialized to None")
+
+    def test_custom_network_key(self):
+        """Test constructor with custom network_key."""
+
+        a = Attribute(network_key='spam')
+
+        self.assertEqual(a.network_key, 'spam', "network_key incorrect")
+        self.assertIsNone(a.name, "name attr not initialized to None")
+
+    def test_set_name(self):
+        """Test setting name after initialization."""
+
+        a = Attribute()
+        a.set_name('attr_name')
+
+        self.assertEqual(a.name, 'attr_name', "name incorrect")
+        self.assertEqual(a.network_key, 'attr-name', "network_key incorrect")
+
+    def test_set_name_with_custom_network_key(self):
+        """Test setting name after initialization with custom network_key."""
+
+        a = Attribute(network_key="custom network key")
+        a.set_name('new_name')
+
+        self.assertEqual(a.name, 'new_name', "name incorrect")
+        self.assertEqual(a.network_key, 'custom network key', "network_key incorrect")
+
+
+class TestAttributeCollection(unittest.TestCase):
+    """Test the AttributeCollection class."""
+
+    def setUp(self):
+        self.a = Attribute()
+        self.a.set_name('my_attribute')
+        self.b = Attribute(network_key='custom network key')
+        self.b.set_name('b_attr')
+
+    def test_attr_collection_basic_functionality(self):
+        """Test adding and retrieving Attribute objects."""
+
+        c = AttributeCollection()
+        c.add(self.a)
+        c.add(self.b)
+
+        self.assertIs(c.get_by_name('my_attribute'), self.a, "failed to retrieve stored Attribute by name")
+        self.assertIs(c.get_by_name('b_attr'), self.b, "failed to retrieve stored Attribute by name")
+        self.assertIs(c.get_by_network_key('my-attribute'), self.a, "failed to retrieve stored Attribute by network key")
+        self.assertIs(c.get_by_network_key('custom network key'), self.b, "failed to retrieve stored Attribute by network key")
+
+    def test_raises_exception_when_adding_invalid_Attribute(self):
+        """Test that an exception is raised when adding an Attribute with an invalid name."""
+
+        c = AttributeCollection()
+        a = Attribute() # No name specified
+
+        with self.assertRaises(ValueError, msg="no exception raised when adding Attribute with no name attr defined"):
+            c.add(a)
+
+    def test_raises_exception_when_adding_duplicate_name(self):
+        """Test that an exception is raised for duplicate name."""
+
+        c = AttributeCollection()
+        a1 = Attribute()
+        a1.set_name('a')
+        a2 = Attribute()
+        a2.set_name('a')
+        c.add(a1)
+
+        with self.assertRaises(ValueError, msg="no exception raised when adding Attribute with duplicate name"):
+            c.add(a2)
+
+    def test_raises_exception_when_adding_duplicate_network_key(self):
+        """Test that an exception is raised for duplicate network_key."""
+
+        c = AttributeCollection()
+        a = Attribute(network_key='dupl')
+        a.set_name('a')
+        b = Attribute(network_key='dupl')
+        b.set_name('b')
+        c.add(a)
+
+        with self.assertRaises(ValueError, msg="no exception raised when adding Attribute with duplicate network_key"):
+            c.add(b)
+
+    def test_iteration(self):
+        """Test that the collection is iterable."""
+
+        c = AttributeCollection()
+        c.add(self.a)
+        c.add(self.b)
+        iterator1 = iter(c)
+        iterator2 = iter(c)
+
+        self.assertEqual(len(c), 2, "AttributeCollection length incorrect")
+        self.assertIs(next(iterator1), self.a, "AttributeCollection iterator not working as expected")
+        self.assertIs(next(iterator1), self.b, "AttributeCollection iterator not working as expected")
+        self.assertIs(next(iterator2), self.a, "AttributeCollection iterator not working as expected")
+        self.assertIs(next(iterator2), self.b, "AttributeCollection iterator not working as expected")
+
+        with self.assertRaises(StopIteration):
+            next(iterator1)
+
+        with self.assertRaises(StopIteration):
+            next(iterator2)
+
+    def test_in_operator(self):
+        """Test that the in operator can be used to test if a name is in the AttributeCollection."""
+
+        c = AttributeCollection()
+        c.add(self.a)
+        c.add(self.b)
+
+        self.assertIn(self.a, c, "membership test with Attribute() instance returned false negative")
+        self.assertIn('my_attribute', c, "membership test with str name returned false negative")
+        self.assertIn(self.b, c, "membership test with Attribute() instance returned false negative")
+        self.assertIn('b_attr', c, "membership test with str name returned false negative")
+        self.assertNotIn(Attribute(), c, "membership test with Attribute() instance returned false positive")
+        self.assertNotIn('definitely missing', c, "membership test with str name returned false positive")
+
+
 class TestResource(unittest.TestCase):
     """Test functionality common to all resources."""
+
+    # Uses the Configuration subclass as a concrete instance
+    # to test base Resource functionality.
 
     def setUp(self):
         self.init_kwargs = {
@@ -12,45 +145,72 @@ class TestResource(unittest.TestCase):
             'primary_ip': '192.168.0.1',
         }
 
+    def test_subclassing_raises_exception_on_missing_resource_type(self):
+        """Test that subclassing Resource without a resource_type attr raises an exception."""
+
+        # No exception expected
+        class Subclass(Resource):
+            class Meta:
+                resource_type = "my_resource"
+
+        no_type_msg = "no exception raised for subclasses without Meta.resource_type defined"
+
+        with self.assertRaises(AttributeError, msg=no_type_msg):
+            class Subclass(Resource): pass
+
+        with self.assertRaises(AttributeError, msg=no_type_msg):
+            class Subclass(Resource):
+                class Meta: pass
+
     def test_constructor_noargs(self):
         """Test initialization with no arguments."""
 
         c = Configuration()
-        self.assertEqual(c.id, None, "id is not None when no args passed to constructor")
+        self.assertIsNone(c.id, "id is not None when no args passed to constructor")
 
     def test_constructor_accepts_id(self):
         """Test initialization with an ID."""
 
         c = Configuration('555')
-        self.assertEqual(c.id, '555', "id not initialized (positional arg)")
+        self.assertEqual(c.id, '555', "id attribute not initialized (positional arg)")
 
     def test_constructor_accepts_kwargs(self):
         """Test initialization with keyword args."""
 
         c = Configuration(**self.init_kwargs)
-        self.assertFalse(hasattr(c, '_network_dict'), "_network_dict is defined when resource is initialized without a network dict")
 
         for (key, val) in self.init_kwargs.items():
             with self.subTest(attr=key):
-                self.assertEqual(getattr(c, key), val, "attribute not initialized")
+                self.assertEqual(getattr(c, key), val, f"{key} attribute not initialized")
 
     def test_constructor_accepts_id_and_kwargs(self):
         """Test initialization with an ID and keyword args."""
 
         c = Configuration('64', **self.init_kwargs)
-        self.assertEqual(c.id, '64', "id not initialized")
-        self.assertFalse(hasattr(c, '_network_dict'), "_network_dict is defined when resource is initialized without a network dict")
+        self.assertEqual(c.id, '64', "id attribute not initialized")
 
         for (key, val) in self.init_kwargs.items():
             with self.subTest(attr=key):
-                self.assertEqual(getattr(c, key), val, "attribute not initialized")
+                self.assertEqual(getattr(c, key), val, f"{key} attribute not initialized")
+
+    def test_constructor_only_defines_kwargs_as_attrs(self):
+        """
+        Test that the constructor only defines attributes on the instance that correspond to the passed kwargs.
+        Even valid attribute names should be undefined unless they were passed in the constructor.
+        """
+
+        c = Configuration(**self.init_kwargs)
+        self.assertFalse(hasattr(c, 'installed_at'), "installed_at was not included in the constructor kwargs but it is defined on the instance")
+
+        c = Configuration(installed_at='2010-05-20', **self.init_kwargs)
+        self.assertEqual(c.installed_at, '2010-05-20', "installed_at attribute not initialized")
 
     def test_constructor_raises_exception_on_invalid_kwargs(self):
         """Test that invalid constructor args raise an exception."""
 
         with self.assertRaises(TypeError, msg="no exception raised on invalid constructor args"):
             Configuration('987', name="My config", invalid_arg="spam")
-    
+
     def test_create_resource_from_network_dict(self):
         """Test creating a resource from a raw network dict."""
 
@@ -64,10 +224,21 @@ class TestResource(unittest.TestCase):
             },
         }
 
-        c = Configuration.from_network_dict(d)
+        c = Resource.from_network_dict(d)
 
+        self.assertIsInstance(c, Configuration, "Resource constructed from network dict is not an instance of the appropriate subclass")
         self.assertEqual(c.id, d['id'], "id attribute not initialized from network dict")
         self.assertEqual(c.name, d['attributes']['name'], "name attribute not initialized from network dict")
         self.assertEqual(c.primary_ip, d['attributes']['primary-ip'], "primary_ip attribute not initialized from network dict")
         self.assertFalse(hasattr(c, 'unknown_attr'), "unknown attribute is defined on resource")
-        self.assertEqual(c._network_dict, d, "_network_dict is not initialized")
+        self.assertEqual(c.Meta.network_dict, d, "Resource.Meta.network_dict is not initialized")
+
+    def test_subclass_lookup(self):
+        """Test finding a subclass by its Meta.resource_type value."""
+
+        class Subclass(Resource):
+            class Meta:
+                resource_type = 'my_subclass'
+
+        self.assertIs(Resource.lookup_subclass('my_subclass'), Subclass, "Resource.lookup_subclass() didn't return the correct class")
+        self.assertIs(Resource.lookup_subclass('configurations'), Configuration, "Resource.lookup_subclass() didn't return the correct class")
